01e128170ad2c1a739b1f1393c7135a521cbf3d9
plasante <pierre.lasante@videotron.ca>
2015-04-15 17:06:47 -0500

    Created interface GreetingDao

--- src/main/java/com/uniksoft/epicerie/service/GreetingService.java
+++ src/main/java/com/uniksoft/epicerie/service/GreetingService.java
@@ -2,37 +2,39 @@ package com.uniksoft.epicerie.service;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.uniksoft.epicerie.dao.GreetingDao;
+import com.uniksoft.epicerie.dao.HibernateGreetingDao;
 import com.uniksoft.epicerie.domain.Greeting;
 
 @Service
 @Transactional
 public class GreetingService {
 
-	@Autowired
-	private GreetingDao greetingDao;
+	@Resource(name="greetingDao")
+	private HibernateGreetingDao hibernateGreetingDao;
 	
 	public void updateGreeting(Greeting greeting) {
-		greetingDao.updateGreeting(greeting);
+		hibernateGreetingDao.updateGreeting(greeting);
 	}
 	
 	public List<Greeting> getAllGreetings() {
-		return greetingDao.getAllGreetings();
+		return hibernateGreetingDao.getAllGreetings();
 	}
 	
 	public void addGreeting(Greeting greeting) {
-		greetingDao.addGreeting(greeting);
+		hibernateGreetingDao.addGreeting(greeting);
 	}
 	
 	public Greeting getGreetingById(Integer greetingId) {
-		return greetingDao.getGreetingById(greetingId);
+		return hibernateGreetingDao.getGreetingById(greetingId);
 	}
 	
 	public void deleteGreeting(Greeting greeting) {
-		greetingDao.deleteGreeting(greeting);
+		hibernateGreetingDao.deleteGreeting(greeting);
 	}
 }
